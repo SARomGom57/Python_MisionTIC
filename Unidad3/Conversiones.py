e0fd74f82becf371808428855065f5849b084d0b
JetChimera <79342491+SARomGom57@users.noreply.github.com>
2022-08-11 22:46:26 -0500

    Ciclo 1 Unidad 3 MisionTIC

---
+++ Unidad3/Conversiones.py
@@ -0,0 +1,19 @@
+# Conversion de cadena a lista
+cadena = 'Colombia'
+lista = list(cadena)
+print(lista)
+
+# Conversion de lista a cadena
+cadena2 = ''.join(lista)
+print(cadena2)
+
+lista2 = [1,5,7,8,9]
+# no se puede porque requiere que los elementos sean str
+
+conjunto = set(lista)
+
+diccionario = dict()
+
+tempo = list(zip(range(len(cadena)), cadena))
+print(tempo)
+
